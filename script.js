8f11a60cb0e238358a72edcb75064408aad091d2
Kabilash Satheesh Kumar <iamkabilash@Kabilashs-MacBook-Pro.local>
2022-10-02 18:51:03 +0530

    updated from chuck norris to dadjokes api

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const jokeElement = document.getElementById("joke");
 const button = document.getElementById("jokeBtn");
 
-async function getJoke() {
-    url = "https://api.chucknorris.io/jokes/random";
-    const joke = await fetch(url).then((response) => response.json());
-    jokeElement.innerText = joke.value;
+// async function getJoke() {
+//     url = "https://api.chucknorris.io/jokes/random";
+//     const joke = await fetch(url).then((response) => response.json());
+//     jokeElement.innerText = joke.value;
+// }
+
+function getJoke() {
+    fetch("https://icanhazdadjoke.com/slack")
+        .then(res => res.json())
+        .then(data => jokeElement.innerText = data.attachments[0].text);
 }
 
 button.addEventListener("click", getJoke);
+
